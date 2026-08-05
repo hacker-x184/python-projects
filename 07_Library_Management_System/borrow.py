baad460e71694e3fc402b1c4b24887fdc2a1303e
Mohd Inzamam <inzamamx184@gmail.com>
2026-08-05 18:16:12 +0530

    At the end of the project

--- 07_Library_Management_System/borrow.py
+++ 07_Library_Management_System/borrow.py
@@ -1,13 +1,35 @@
 from database import db_query
-def borrow_book(isbn,qn):
+def borrow_book(isbn,member_id,qn):
     title = db_query(f"SELECT title FROM book WHERE isbn = '{isbn}';")
     db_query(f"UPDATE book SET quantity={qn} WHERE isbn = '{isbn}'; ")
     print(f"Now you borrow the {title[0][0]}")
-    db_query(f"")
-    
+    book_id  = db_query(f"SELECT book_id FROM book WHERE isbn = '{isbn}';")
+    db_query(f"INSERT INTO borrow(book_id,member_id,due_date,status)VALUES ({book_id},{member_id},CURRENT_DATE + 7,TRUE);")
+def return_book(book_id):
+    try:            
+        temp = db_query(f"SELECT * FROM borrow WHERE book_id = {book_id};")
+        if temp:
+            db_query(f"UPDATE borrow SET return_date = CURRENT_DATE,status = FALSE WHERE book_id = {temp[0][0]};")
+            print("Your book return successful")
+        else:
+            print("Your book id are not Avaiable")
+    except Exception as e:
+        print(e)
 
-
-
+def borrow_history(member_id):
+    try:
+        temp = db_query(f"SELECT * FROM borrow WHERE member_id = {member_id};")
+        print("here is your history:-n",temp)
+    except Exception as e:
+        print(e)
+def check_overdue():
+    try:
+        temp = db_query(f"SELECT * FROM borrow WHERE due_date < CURRENT_DATE;")
+        print("Here is th data of the member of the overdue book")
+        for due in temp:
+            print(due)
+    except Exception as e:
+        print(e)
 """borrow_book()
 
 return_book()
--- 07_Library_Management_System/members.py
+++ 07_Library_Management_System/members.py
@@ -9,17 +9,46 @@ class Members:
         try:
             db_query(f"""
                     INSERT INTO member(name,phone,city) VALUES(
-                        '{self.__name}','{self.__phone}','{self.__city}')""")
+                        '{self.__name}','{self.__phone}','{self.__city}');""")
             print("Now you are the member of library")
         except Exception as e:
             print(e)
     def remove_member(self,member_id):
         try:
-            db_query(f"""DELECT * FROM member WHERE member_id  = {member_id}
+            db_query(f"""DELECT FROM member WHERE member_id  = {member_id};
                      """)
         except Exception as e:
             print(e)
+    def update_member(self,member_id,name):
+        try:
+            temp = db_query(f"""SELECT member_id FROM member WHERE member_id = {member_id};""")
+            if temp:
+                db_query(f"""UPDATE member
+                SET name = '{name}'
+                WHERE member_id = {member_id};""")
+            else:
+                print("Your member_id not available in data :-)")
+        except Exception as e:
+            print(e)
+    def display_member(self):
+            try:
+                member_data = db_query(f"""SELECT * FROM member;""")
+                print(f"Here are the list of the members:-")
+                for member in member_data:
+                    print(member)                   
 
+            except Exception as e:
+                print(e)
+    def search_member(self,member_id):
+        try:
+            temp = db_query(f"SELECT * FROM member WHERE member_id = {member_id};")
+            if temp:
+                print("member are avaiable in List\nHere is your data")
+                print(temp)
+            else:
+                print("Your member_id not available in data :-)")
+        except Exception as e:
+            print(e)
 """add_member()
 
 remove_member()
